da3ac67d8bf00240667fc0ff699b6c1361ced639
David Botello <dabocher@gmail.com>
2023-09-12 09:05:39 +0200

    Fix size and colors from sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import Sidevar from "@/components/Sidevar";
+import Header from "@/components/Header";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
@@ -17,8 +18,10 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="bg-[#E1Dadb]">
-      <body className={`${quicksand.className} relative pt-24 bg-[#E1Dadb] `}>
+      <body className={`${quicksand.className} relative pt-8  bg-[#E1Dadb] `}>
+        <Header />
         <Sidevar />
+
         {children}
         <Footer />
       </body>
